ae6f1a815d81d3508f4268d83fd4178abef39561
Vlad <Saphire.g8@gmail.com>
2023-06-03 23:05:54 +0200

    delete unnecessary files

--- config.h
+++
@@ -1,8 +0,0 @@
-#ifndef ISPRINT_H
-#define ISPRINT_H
-//**********************************************************************************************
-//0 - to not view array before and after sorting
-//1 - to view array before and after sorting
-#define isPrintArray 0
-
-#endif
